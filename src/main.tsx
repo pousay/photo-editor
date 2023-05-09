a6e725c6b00dca531179bacf738823cef1774461
pousay <pouryathesaymon@gmail.com>
2023-05-10 02:30:07 +0330

    deploy

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import ReactDOM from "react-dom/client";
+import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import App from "./App";
 import store from "./components/reducers/store";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+createRoot(document.getElementById("root") as HTMLElement).render(
   <Provider store={store}>
     <App />
   </Provider>
